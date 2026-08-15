93fe4b109063c985c85120300352a2ec7ba01d31
adijha7 <adijha6205@gmail.com>
2026-08-15 20:46:28 +0530

    First type of variable

---
+++ Type_of_Variable.java
@@ -0,0 +1,18 @@
+// first example of variable types in java
+
+public class Type_of_Variable {
+    
+
+    public static void main(String[] args) {
+       
+        Type_of_Variable obj = new Type_of_Variable();
+        obj.myMethod();
+    }
+
+    public void myMethod() {
+        int localVariable = 10; // Local variable
+        System.out.println("Local Variable: " + localVariable);
+    }
+
+    
+}
