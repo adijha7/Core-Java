3c9a94b9918415162108e7626c205d4fe0a8de64
adijha7 <adijha6205@gmail.com>
2026-08-18 21:29:32 +0530

    new file added

---
+++ calcultor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class calcultor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+          sc.nextInt();
+        System.out.println("Enter the second number: ");
+          sc.nextInt();
+        
+    
+}
