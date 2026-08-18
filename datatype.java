33eb30b9629911ee81717b2a1020e6bb2a57042d
adijha7 <adijha6205@gmail.com>
2026-08-18 21:21:35 +0530

    updates

--- datatype.java
+++ datatype.java
@@ -1,9 +1,11 @@
 public class datatype {
      public static void main(String[] args) {
         int a=10;
+        char c='A';
         String str="Aditya";
         Float f=10.32f;
         boolean g=true;
+        system.out.println("Character: " + c);
        System.out.println("Integer: " + a);
         System.out.println("String: " + str); 
         System.out.println("Float: " + f);
