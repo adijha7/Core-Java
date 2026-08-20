0dfbdd106f29fdf072394568fbb7e4adfce508de
adijha7 <adijha6205@gmail.com>
2026-08-20 08:53:12 +0530

    Started learing Java

---
+++ part1/IdentifierExample.java
@@ -0,0 +1,12 @@
+package part1;
+
+public class IdentifierExample {
+	public static void main (String args []) {
+		System.out.println(10);
+		System.out.println(20);
+		System.out.print(30);
+		System.out.println(40);
+		System.out.print(50);
+	}
+
+}
