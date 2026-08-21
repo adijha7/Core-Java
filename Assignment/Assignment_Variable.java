e902074ba87a9e5b834d84434d70a2b736c4b7fb
adijha7 <adijha6205@gmail.com>
2026-08-21 19:33:37 +0530

    Datatype

---
+++ Assignment/Assignment_Variable.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+public class Assignment_Variable {
+	public static void main(String[] args) {
+		byte age=13;
+		String name="Smith";
+		byte Standard=9;
+		float percentage=95.75f;
+		char Class='B';
+		System.out.println("The Age is :- "+age);
+		System.out.println("The Name is :- "+name);
+		System.out.println("The Standard is :- "+Standard);
+		System.out.println("The Percentage is :-"+percentage);
+		System.out.println("The Class is :-  "+Class);
+		
+	}
+
+}
