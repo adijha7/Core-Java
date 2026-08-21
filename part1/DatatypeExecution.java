f0a4dd16df954e44330d6898252c781ed05f4a35
adijha7 <adijha6205@gmail.com>
2026-08-21 19:36:39 +0530

    new file

---
+++ part1/DatatypeExecution.java
@@ -0,0 +1,37 @@
+package part1;
+
+public class DatatypeExecution {
+    public static void main(String[] args) {
+		
+		
+		System.out.println(" The min and Max value of Short ");
+		System.out.println(Short.MIN_VALUE );
+		System.out.println(Short.MAX_VALUE );
+		
+		System.out.println(" The min and Max value of Integer ");
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		
+		System.out.println(" The min and Max value of Long ");
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+		System.out.println(" The min and Max value of Float ");
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		
+		System.out.println(" The min and Max value of Double ");
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		System.out.println(" The min  value of Character ");
+		System.out.println(Character.MIN_VALUE);
+		
+		
+		
+		
+	}
+
+    
+}
