4c8fe2dbab38fa43cda46b8a25920ddac5711555
adijha7 <adijha6205@gmail.com>
2026-08-22 09:34:11 +0530

    operator file is add

---
+++ operatorexecution/ArithematicOperatorExample.java
@@ -0,0 +1,13 @@
+package operatorexecution;
+
+public class ArithematicOperatorExample {
+    public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int add=a+b;
+		System.out.println(add); // Addition of a + b : 30
+		System.out.println("Addition of a + b :"+add);
+		// Addition of 10 + 20 : 30
+		System.out.println("Addition of "  +a+  "+" + b + ":" +add);
+	}
+}
