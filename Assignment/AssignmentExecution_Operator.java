3572860df9e0bdef8f765edad531e17330177b45
adijha7 <adijha6205@gmail.com>
2026-08-22 09:42:33 +0530

    New Assignment

---
+++ Assignment/AssignmentExecution_Operator.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class AssignmentExecution_Operator {
+    public static void main(String[] args) {
+		
+		
+		int id=101;
+		String name="Smith";
+		int age=30;
+		String gender="Male";
+		
+		System.out.println("the Data of Student is :- ");
+		System.out.println("Student Id :"+id);
+		System.out.println("Student Name :"+name);
+		System.out.println("Student Age :"+age);
+		System.out.println("Student Gender :"+gender);
+	}
+    
+}
