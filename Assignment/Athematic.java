33d90525d3581d5dd2fbe52bd52afcbd4d3af2dd
adijha7 <adijha6205@gmail.com>
2026-08-23 10:24:47 +0530

    new file

---
+++ Assignment/Athematic.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+public class Athematic {
+    
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=20;
+        
+        System.out.println("The Addition of a and b is :"+(a+b));
+        System.out.println("The Subtraction of a and b is :"+(a-b));
+        System.out.println("The Multiplication of a and b is :"+(a*b));
+        System.out.println("The Division of a and b is :"+(a/b));
+        System.out.println("The Modulus of a and b is :"+(a%b));
+        
+    }
+
+    
+
+    
+    
+}
