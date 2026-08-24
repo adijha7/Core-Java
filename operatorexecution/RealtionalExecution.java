a1880e075578e22991ccad6f7d6ffdd632876862
adijha7 <adijha6205@gmail.com>
2026-08-24 19:19:27 +0530

    updated new file

---
+++ operatorexecution/RealtionalExecution.java
@@ -0,0 +1,26 @@
+package operatorexecution;
+
+public class RealtionalExecution {
+    public static void main(String[] args) {
+		
+          /*   */
+		char size='M';
+		/*   */
+		char t1='l';
+		char t2='S';
+		char t3='M';
+		/*Test Case 1   */
+		boolean testcase1=size==t1;
+		System.out.println(testcase1);
+		
+		/*Test Case 2  */
+		boolean testcase2=size==t2;
+		System.out.println(testcase2);
+		
+		/*Test Case 3  */
+		boolean testcase3=size==t3;
+		System.out.println(testcase3);
+	}
+
+    
+}
