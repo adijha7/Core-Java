aafc9caa4c6ccba436f539a863b2d7e5d3023929
adijha7 <adijha6205@gmail.com>
2026-08-27 21:00:01 +0530

    file

---
+++ operatorexecution/BasedonDatatype.java
@@ -0,0 +1,23 @@
+package operatorexecution;
+
+public class BasedonDatatype {
+    public static void main(String[] args) {
+		
+		// Primitive Variable declaration and in
+		int a=10;
+		System.out.println(a);
+		
+		// non Primitive Variable declaration and in
+		BasedonDatatype b=null;
+		System.out.println(b);
+		
+		String s=null;
+		System.out.println(s);
+		
+		double d=0.0d;
+		System.out.println(d);
+		
+		
+	}
+    
+}
