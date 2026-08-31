b607948bd55e23a17cf6304c75fcf5d65c9ae218
adijha7 <adijha6205@gmail.com>
2026-08-31 09:24:36 +0530

    new file

---
+++ LoopingStatement/Forloop_Example.java
@@ -0,0 +1,11 @@
+package LoopingStatement;
+
+public class Forloop_Example {
+	public static void main(String[] args) {
+	
+		for(int num=1;num<=5;num++) {
+			System.out.println(num);
+		}
+	}
+
+}
