2302fbc6c2fc214ee05fbb15fb946feb6e99b10a
adijha7 <adijha6205@gmail.com>
2026-09-01 08:41:42 +0530

    new file

---
+++ LoopingStatement/DoWhileExecution.java
@@ -0,0 +1,14 @@
+package LoopingStatement;
+
+public class DoWhileExecution {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+			
+		}while(i<=5);
+		
+	}
+
+}
---
+++ LoopingStatement/whileloop_Example.java
@@ -0,0 +1,12 @@
+package LoopingStatement;
+
+public class whileloop_Example {
+ public static void main(String[] args) {
+	int num=0;
+	while(num<=10) {
+		System.out.println(num);
+		num++;
+	}
+}
+
+}
