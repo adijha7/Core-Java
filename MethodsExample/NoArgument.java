90579c6bbe03fa74b3b6926a6dc3b2cdab5e985e
adijha7 <adijha6205@gmail.com>
2026-09-02 09:42:43 +0530

    new file

---
+++ MethodsExample/NoArgument.java
@@ -0,0 +1,14 @@
+package MethodsExample;
+
+public class NoArgument {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		add();
+	}
+
+}
