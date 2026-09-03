ff197aa3692432649a848049503a90f254162135
adijha7 <adijha6205@gmail.com>
2026-09-03 20:07:41 +0530

    new file

--- MethodsExample/MethodExecutionExaample.java
+++ MethodsExample/MethodExecutionExaample.java
@@ -10,6 +10,8 @@ public class MethodExecutionExaample {
 public static void main(String[] args) {
 	System.out.println("----Main Start------");
 	display();
+	test();
+	System.out.println("---- Main End ------ ");
 	
 }
 }
