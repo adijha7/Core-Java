020fb64a0935be6daf6036e0ae4fb32a5131d851
adijha7 <adijha6205@gmail.com>
2026-09-04 09:18:01 +0530

    new file

---
+++ MethodsExample/ParameterizedMethodExample.java
@@ -0,0 +1,12 @@
+package MethodsExample;
+
+public class ParameterizedMethodExample {
+	public static void add(int a,int b) {
+		int sum=a+b;
+		System.out.println("Addition is "+a+" is "+b+" : "+sum);
+	}
+	public static void main(String[] args) {
+		add(10,20);
+	}
+
+}
