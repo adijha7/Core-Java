c3b3a951befd85aebbb71dc449d66f69744330f3
adijha7 <adijha6205@gmail.com>
2026-09-05 21:15:58 +0530

    new file

---
+++ Assignment/numberispostiveornot.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class numberispostiveornot {
+	public static void check(int a) {
+		if(a>0) {
+		 System.out.println(a+ " is Postive");
+		}
+		else {
+			System.out.println(a+"is Negative");
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		check(55);
+	}
+	
+
+}
