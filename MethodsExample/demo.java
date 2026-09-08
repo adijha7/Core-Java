b5c6b28b336e79e5032995cf8f854bdbd69b7d6e
adijha7 <adijha6205@gmail.com>
2026-09-08 09:18:29 +0530

    new file

---
+++ MethodsExample/demo.java
@@ -0,0 +1,23 @@
+package MethodsExample;
+
+
+// it for static method 
+
+
+ class demodriver
+{
+	public static void m1() 
+	{
+		System.out.println("Hello from m1");
+	}
+}
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("------Main Start-----");
+		demodriver.m1();
+//		demodriver a= new demodriver();
+//		a.m1();
+		System.out.println("------Main Start-----");
+	}
+
+}
