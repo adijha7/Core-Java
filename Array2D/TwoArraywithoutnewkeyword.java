7c33bf911a087a04b5efde26da7fd5be70f1f632
adijha7 <adijha6205@gmail.com>
2026-09-15 08:43:30 +0530

    new file

---
+++ Array2D/TwoArraywithoutnewkeyword.java
@@ -0,0 +1,15 @@
+package Array2D;
+
+public class TwoArraywithoutnewkeyword {
+	public static void main(String[] args) {
+		int [][]arr= {{1,2,3,},{4,5,6},{7,8,9}};
+		System.out.println(arr.length);
+		System.out.println(arr[0]);
+		System.out.println(arr[0].length);
+		System.out.println(arr [0][1]);
+		System.out.println(arr[2].length);
+		
+		
+	}
+
+}
